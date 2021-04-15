6ddc7b95ec5d3bdf59b2a24c089f6a80c1ac7314
Douglas Little <46497179+dlittle-525@users.noreply.github.com>
2021-04-14 20:51:05 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // require your server and launch it
 const server = require("./api/server")
 
-const port = 5000
+const port = process.env.PORT || 5000
 
 server.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`)
